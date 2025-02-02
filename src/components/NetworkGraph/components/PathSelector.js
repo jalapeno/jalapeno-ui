363c87c21e3459a14ebc1c6f523da35d7dcacedb
brmcdoug <brmcdoug@cisco.com>
2025-02-02 12:04:15 -0800

    cleanup

---
+++ src/components/NetworkGraph/components/PathSelector.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import PathControls from './PathControls';
+
+const PathSelectorContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 10;
+  background: rgba(255, 255, 255, 0.9);
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+`;
+
+const PathSelector = ({ 
+  onConstraintChange,
+  onClear,
+  disabled 
+}) => {
+  return (
+    <PathSelectorContainer>
+      <PathControls 
+        onConstraintChange={onConstraintChange}
+        onClear={onClear}
+        disabled={disabled}
+      />
+    </PathSelectorContainer>
+  );
+};
+
+export default PathSelector; 
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Development API URL (using k8s-deployed API)
-//const apiUrl = 'http://198.18.133.111:30800';
+const apiUrl = 'http://198.18.128.101:30800';
 //const apiUrl = 'http://localhost:8000';
 
 // Production API URL from environment variable
-const apiUrl = process.env.REACT_APP_API_URL;
+//const apiUrl = process.env.REACT_APP_API_URL;
 
 export { apiUrl }; 
