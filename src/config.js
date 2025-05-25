985ba0ac89850a3dd3307a6f50a160f82786f30a
brmcdoug <brmcdoug@cisco.com>
2025-05-24 21:06:33 -0700

    api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Development API URL (using k8s-deployed API)
-const apiUrl = 'http://198.18.128.101:30800';
-//const apiUrl = 'http://localhost:8000';
+//const apiUrl = 'http://198.18.128.101:30800';
+const apiUrl = 'http://localhost:8000';
 
 // Production API URL from environment variable
 //const apiUrl = process.env.REACT_APP_API_URL;
